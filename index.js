23c54d99126705eeb7ee4ba68459e04dd1162fb0
UditNayak <uditnayak027@gmail.com>
2024-07-03 11:36:58 +0530

    CRUD Operations on File

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
 app.use(express.json());
-app.use(customLogger);
-
 
 // Custom logger middleware
 function customLogger(req, res, next) {
@@ -11,51 +11,58 @@ function customLogger(req, res, next) {
         `Method: ${req.method}, IP: ${req.ip}, Hostname: ${req.hostname}, Date: ${new Date()}`
     );
     next();
-};
+}
+app.use(customLogger);
 
+const coursesFilePath = path.join(__dirname, 'courses.json');
 
-let courses = [
-  { id: 1, name: "course1" },
-  { id: 2, name: "course2" },
-  { id: 3, name: "course3" },
-];
+// Utility function to read courses from the file
+function readCoursesFromFile() {
+    const data = fs.readFileSync(coursesFilePath, 'utf-8');
+    return JSON.parse(data);
+}
+
+// Utility function to write courses to the file
+function writeCoursesToFile(courses) {
+    fs.writeFileSync(coursesFilePath, JSON.stringify(courses, null, 2));
+}
 
 app.get("/courses", (req, res) => {
-  res.json(courses);
+    const courses = readCoursesFromFile();
+    res.json(courses);
 });
 
 app.post("/courses", (req, res) => {
-  console.log(req.body);
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name,
-  };
-  courses.push(course);
-  res.json(course);
+    const courses = readCoursesFromFile();
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name,
+    };
+    courses.push(course);
+    writeCoursesToFile(courses);
+    res.json(course);
 });
-
-// update the id to spring boot
 
 app.put("/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course)
-    return res.status(404).send("The course with the given ID was not found");
+    const courses = readCoursesFromFile();
+    const course = courses.find((c) => c.id === parseInt(req.params.id));
+    if (!course)
+        return res.status(404).send("The course with the given ID was not found");
 
-  course.name = req.body.name;
-  res.json(course);
+    course.name = req.body.name;
+    writeCoursesToFile(courses);
+    res.json(course);
 });
 
-// delete the id to spring boot
-
 app.delete("/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course)
-    return res.status(404).send("The course with the given ID was not found");
+    let courses = readCoursesFromFile();
+    const course = courses.find((c) => c.id === parseInt(req.params.id));
+    if (!course)
+        return res.status(404).send("The course with the given ID was not found");
 
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-
-  res.json(course);
+    courses = courses.filter((c) => c.id !== parseInt(req.params.id));
+    writeCoursesToFile(courses);
+    res.json(course);
 });
 
 app.listen(3000, () => console.log("Server Started on port 3000..."));
