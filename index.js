9f33a4c26dba044bf453d3fc5c360e885d02c51c
UditNayak <uditnayak027@gmail.com>
2024-07-03 10:24:45 +0530

    Post Request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let courses = [
     { id: 1, name: 'course1' },
     { id: 2, name: 'course2' },
@@ -9,6 +11,16 @@ let courses = [
 
 app.get('/courses', (req, res) => {
     res.json(courses);
+});
+
+app.post('/courses', (req, res) => {
+    console.log(req.body);
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.json(course);
 });
 
 app.listen(3000, () => console.log('Server Started on port 3000...'));
